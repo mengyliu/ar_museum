33a2001805c5d55bfdfcfed8c0e7332c42551e65
mengyliu <mengyliu@umich.edu>
2019-02-23 19:26:36 -0500

    add js

--- js/script.js
+++ js/script.js
@@ -82,4 +82,24 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     }
   });
+
+  var mappings = {
+    mappings: {
+    default: {
+      common: {
+        trackpaddown: 'teleportstart',
+        trackpadup: 'teleportend'
+      },
+      'oculus-touch-controls': {
+        thumbstickdown: 'teleportstart',
+        thumbstickup: 'teleportend'
+      },
+      keyboard: {
+        't_down': 'teleportstart',
+        't_up': 'teleportend'
+      }
+    }
+    }
+  };
+  AFRAME.registerInputMappings(mappings);
 });
