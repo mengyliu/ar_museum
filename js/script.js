16efbca47f110eeb66a0765e285990a985f441e3
mengyliu <mengyliu@umich.edu>
2019-02-23 19:54:10 -0500

    changed back to simple code

--- js/script.js
+++ js/script.js
@@ -82,24 +82,4 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     }
   });
-
-  var mappings = {
-    mappings: {
-    default: {
-      common: {
-        trackpaddown: 'teleportstart',
-        trackpadup: 'teleportend'
-      },
-      'oculus-touch-controls': {
-        thumbstickdown: 'teleportstart',
-        thumbstickup: 'teleportend'
-      },
-      keyboard: {
-        't_down': 'teleportstart',
-        't_up': 'teleportend'
-      }
-    }
-    }
-  };
-  AFRAME.registerInputMappings(mappings);
 });
